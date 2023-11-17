643d41826cd65ca155182cfc0e4342c8c57020fd
Charles Wyble <charles@turnsys.com>
2023-11-17 12:42:30 -0600

    things and stuff. guess i should make a .gitignore...

--- generateResume.sh
+++ generateResume.sh
@@ -1,39 +1,66 @@
 #!/bin/bash
 
-# A script to generate shareable resume file
+# A script to generate pdf/doc resume file
+# Two versions
+# Comprehensive (more for long form reading by humans)
+# Chronological (more for machine consumption/short form reading for humans)
+
+#Globals
+
+export ai1_resume_file="AllInOneResume.md"
+export ai1_PDF_OUTPUT_FILE="CharlesNWyble-December2023-ComprehensiveResume.pdf"
+export ai1_DOC_OUTPUT_FILE="CharlesWyble-December2023-ComprehensiveResume.docx"
+
+export chron_resume_file="ChronologicalResume.md"
+export chron_PDF_OUTPUT_FILE="CharlesNWyble-December2023-ChronologicalResume.pdf"
+export chron_DOC_OUTPUT_FILE="CharlesWyble-December2023-ChronologicalResume.docx"
 
 
 function prevRunCleanup()
 {
- 
-rm $MD_OUTPUT_FILE
-rm $PDF_OUTPUT_FILE
-rm $DOC_OUTPUT_FILE
+echo "Cleaning up all in one resume output files..."
+rm $ai1_DOC_OUTPUT_FILE
+rm $ai1_PDF_OUTPUT_FILE
 
+echo "Cleaning up chronological resume output files..."
+rm $chron_DOC_OUTPUT_FILE
+rm $chron_PDF_OUTPUT_FILE
 }
 
 function makeOutput()
-
 {
 
-resume_file="AllInOneResume.md"
+echo "Creating all in one PDF..."
 
 pandoc \
-	< $resume_file \
+	< $ai1_resume_file \
+    --from=markdown \
+	--output=$ai1_DOC_OUTPUT_FILE
+
+echo "Creating all in one DOC..."
+
+pandoc \
+	< $ai1_resume_file \
     --from=markdown \
 	-V geometry:margin=0.0in \
-	--output=$PDF_OUTPUT_FILE
+	--output=$ai1_PDF_OUTPUT_FILE
+
+echo "Creating chronological DOC..."
 
 pandoc \
-	< $resume_file \
+	< $chron_resume_file \
     --from=markdown \
-	--output=$DOC_OUTPUT_FILE
-}
+	--output=$chron_DOC_OUTPUT_FILE
 
-FILE_NAME="AllInOneResume.md"
-PDF_OUTPUT_FILE="$FILE_NAME-Resume.pdf"
-DOC_OUTPUT_FILE="$FILE_NAME-Resume.docx"
-MD_OUTPUT_FILE="$FILE_NAME-Resume.md"
+echo "Creating chronological PDF..."
+
+pandoc \
+	< $ai1_resume_file \
+    --from=markdown \
+	-V geometry:margin=0.0in \
+	--output=$chron_PDF_OUTPUT_FILE
+
+}
 
 prevRunCleanup
 makeOutput
